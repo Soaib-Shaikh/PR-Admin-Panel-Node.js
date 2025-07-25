31c9658d53b0b26fffaa1ffe8b95065e153898f6
Soaib-Shaikh <samshaikh1192@gmail.com>
2025-07-25 16:26:32 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.port.env || 3000;
+const port = process.env.port || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
